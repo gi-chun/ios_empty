26da43fda0a18d1bab379e8771a52e0a55ca543f
gclee <springgclee@gmail.com>
2015-10-21 17:17:23 +0900

    ttt

--- gcleeEmpty/AppDelegate.m
+++ gcleeEmpty/AppDelegate.m
@@ -19,7 +19,7 @@
     // Override point for customization after application launch.
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor magentaColor];
     [self.window makeKeyAndVisible];
     
     self.mainViewController = [[mainViewController alloc] init];
