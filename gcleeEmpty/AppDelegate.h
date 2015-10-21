40e22e4d8b65d26dfd80f64525026b779dc8f018
gclee <springgclee@gmail.com>
2015-10-21 17:12:14 +0900

    first

---
+++ gcleeEmpty/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  gcleeEmpty
+//
+//  Created by gclee on 2015. 10. 21..
+//  Copyright © 2015년 gclee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "mainViewController.h"
+
+@class mainViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) mainViewController *mainViewController;
+
+@end
+
